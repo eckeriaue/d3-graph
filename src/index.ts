e61de2cdcda582d9f16b6790c150157c23f20381
Elberd Chakhkiev <114188050+eckeriaue@users.noreply.github.com>
2022-10-29 19:51:09 +0300

    path bezier

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ class Graph {
   yLabelWrapp: d3.Selection<HTMLDivElement, unknown, HTMLElement, any>
   xLabelWrapp: d3.Selection<HTMLDivElement, unknown, HTMLElement, any>
   private rContent:  d3.Selection<HTMLDivElement, unknown, HTMLElement, any>
+  private path: d3.Path = d3.path()
   constructor(wrapp: string,
     
     public config: {
@@ -21,31 +22,29 @@ class Graph {
   }
 
   private init() {
-    // this.eachLabel(this.config.yLabel, this.yLabelWrapp)
-    // this.eachLabel(this.config.xLabel, this.xLabelWrapp)
-    this.render(this.scene)
+    this.render()
+  }
+  
+  
+  private render() {
+    this.drawGrid(this.scene)
   }
 
-
-  private eachLabel(arr: string[], wrapp: d3.Selection<HTMLDivElement, unknown, HTMLElement, any>) {
-    this.scene.append('g')
-  }
-
-  private render(scene: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>) {
-    let cols: any  = []
-    const gridGroup = scene.append('g')
+  private drawGrid(scene: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>) {
+    let cols: any[]  = []
+    const gridGroup = scene.append('g').attr('id', 'grid')
     for (let i = 0; i < this.config.xLabel.length; i++) {      
       cols.push(gridGroup.append<"g">('g'))
     }
     const drawGrid = (x: number, y: number) => {
-      cols[x].attr('transform', `translate(${x * 56}, 0)`)
       cols[x]
+      .attr('transform', `translate(${x * 56}, 0)`)
+      .attr('stroke', 'skyblue')
+      .attr('fill', 'none')
+      .attr('stroke-alignment', 'inner')
       .append('rect')
       .attr('width', 56)
       .attr('height', 56)
-      .attr('fill', 'none')
-      .attr('stroke', 'skyblue')
-      .attr('stroke-alignment', 'inner')
       .attr('x', 0)
       .attr('y', y * 56)
     }
@@ -53,7 +52,10 @@ class Graph {
       for (let y = 0; y < this.config.yLabel.length; y++) drawGrid(x,y)
     }
 
-    this.scene.attr('style', `height: ${this.config.yLabel.length * 56}px; width: ${this.config.xLabel.length * 56}px`)
+    this.scene.attr('style', `
+      height: ${this.config.yLabel.length * 56}px;
+      width: ${this.config.xLabel.length * 56}px;
+    `)
 
   }
 
@@ -61,5 +63,5 @@ class Graph {
 
 new Graph('#graph', {
   yLabel: ['15 000', '10 000', '5 000', '0', '-5 000'],
-  xLabel: ["Янв' 21", "Фев' 21", "Мар' 21", "Апр' 21", "Май' 21", "Июн' 21", "Июн' 21","Авг' 21", "Сен' 21", "Окт' 21", "Ноя' 21", "Дек' 21"] ,
+  xLabel: ["Янв' 21", "Фев' 21", "Мар' 21", "Апр' 21", "Май' 21", "Июн' 21", "Июн' 21","Авг' 21", "Сен' 21", "Окт' 21", "Ноя' 21", "Дек' 21"],
 })

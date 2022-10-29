2b75b9f7383d25ef326584870277a8e0381fe9dc
Elberd Chakhkiev <114188050+eckeriaue@users.noreply.github.com>
2022-10-29 21:26:07 +0300

    set init is public method

--- src/index.ts
+++ src/index.ts
@@ -27,10 +27,10 @@ class Graph {
         left: 0,
       }
       
-      this.init()
+      // this.init()
   }
 
-  private init() {
+  init() {
     this.render()
   }
   
@@ -110,7 +110,8 @@ class Graph {
   }
 }
 
-new Graph('#graph', {
+const graph = new Graph('#graph', {
   yLabel: ['15 000', '10 000', '5 000', '0', '-5 000'],
   xLabel: ["Янв' 21", "Фев' 21", "Мар' 21", "Апр' 21", "Май' 21", "Июн' 21", "Июн' 21","Авг' 21", "Сен' 21", "Окт' 21", "Ноя' 21", "Дек' 21"],
 })
+graph.init()

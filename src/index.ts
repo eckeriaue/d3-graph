8c5207bd8bdb10e430c5fb679bab9e16f70ea695
Elberd Chakhkiev <114188050+eckeriaue@users.noreply.github.com>
2022-10-29 20:52:19 +0300

    scene updates

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ class Graph {
   scene: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>
   yLabelWrapp: d3.Selection<HTMLDivElement, unknown, HTMLElement, any>
   xLabelWrapp: d3.Selection<HTMLDivElement, unknown, HTMLElement, any>
+  gridGroup: d3.Selection<SVGGElement, unknown, HTMLElement, any>
   private rContent:  d3.Selection<HTMLDivElement, unknown, HTMLElement, any>
   private path: d3.Path = d3.path()
+  private indent: {top: number,  right: number, bottom: number, left: number,  }
   constructor(wrapp: string,
     
     public config: {
@@ -15,9 +17,15 @@ class Graph {
     
     ) {
 
+
       this.wrapp = d3.select(wrapp).attr('class', 'flex')
       this.rContent = this.wrapp.append('div').attr('class', 'flex flex-col')
       this.scene = this.rContent.append('svg').attr('id', 'scene')
+      this.gridGroup = this.scene.append('g').attr('id', 'grid')
+
+      this.indent = {
+        top: 0,  right: 0, bottom: 0, left: 0,
+      }
       this.init()
   }
 
@@ -27,14 +35,29 @@ class Graph {
   
   
   private render() {
+
+    this.renderYLabel()
+    this.renderXLabel()
+
+    this.scene.attr('style', `
+      height: ${(this.config.yLabel.length * 56) + this.indent.bottom}px;
+      width: ${(this.config.xLabel.length * 56) + this.indent.left}px;
+    `)
+
     this.drawGrid(this.scene)
+    
   }
 
   private drawGrid(scene: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>) {
     let cols: any[]  = []
-    const gridGroup = scene.append('g').attr('id', 'grid')
-    for (let i = 0; i < this.config.xLabel.length; i++) {      
-      cols.push(gridGroup.append<"g">('g'))
+    
+    const
+    xLabel = this.config.xLabel,
+    yLabel = this.config.yLabel
+    
+    
+    for (let i = 0; i < xLabel.length; i++) {      
+      cols.push(this.gridGroup.append<"g">('g'))
     }
     const drawGrid = (x: number, y: number) => {
       cols[x]
@@ -48,17 +71,42 @@ class Graph {
       .attr('x', 0)
       .attr('y', y * 56)
     }
-    for (let x= 0; x < this.config.xLabel.length; x++) {
-      for (let y = 0; y < this.config.yLabel.length; y++) drawGrid(x,y)
+    for (let x= 0; x < xLabel.length; x++) {
+      for (let y = 0; y < yLabel.length - 1; y++) drawGrid(x,y)
     }
 
-    this.scene.attr('style', `
-      height: ${this.config.yLabel.length * 56}px;
-      width: ${this.config.xLabel.length * 56}px;
-    `)
 
   }
 
+  private renderYLabel() {
+    this.indent.left = 56
+    this.gridGroup.attr('transform', `translate(${this.indent.left}, ${56})`)
+    const labelWrapper = this.scene.append('g')
+    
+    for (let i = 0; i < this.config.yLabel.length; i++) {
+      labelWrapper.append('text')
+      .text(this.config.yLabel[i])
+      .attr('x', 56 - 10)
+      .attr('y', i * 56 + 56)
+      .attr('text-anchor', 'end')
+      .attr('font-size', '10px')
+    }
+
+  }
+
+
+  private renderXLabel() {
+    this.indent.bottom = 56
+    const group = this.scene.append('g')
+    group.attr('transform', `translate(${this.indent.left}, ${(56 * this.config.yLabel.length) + 20 })`)
+    for (let i = 0; i < this.config.xLabel.length; i++) {
+      group.append('text')
+      .text(this.config.xLabel[i])
+      .attr('y', 0)
+      .attr('x', i * 56)
+      .attr('font-size', '10px')
+    }
+  }
 }
 
 new Graph('#graph', {

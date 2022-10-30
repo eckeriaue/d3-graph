4885227e0013033467dcbff9cccbf79c55ba7da3
Elberd Chakhkiev <114188050+eckeriaue@users.noreply.github.com>
2022-10-30 18:37:26 +0300

    created method drawGrid

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 import Chart from "./ts/chart.name";
 
-const spline = new Chart.Spline('#343', {})
+const spline = new Chart.Spline('#graph', {
+  
+  xLabel: ['test', 'test2', 'test3', 'test4'],
+  yLabel: ['test', 'test2', 'test3', 'test4'],
+  grid: {
+    size: 56,
+  },
+  asymptotes: [
+    {
+      points: [
+        { x: 0, y: 0 },
+        { x: 2, y: 4 },
+      ],
+      color: '#000',
+    },
+  ]
+})
+spline.render()
--- src/ts/chart.name.ts
+++ src/ts/chart.name.ts
@@ -1,27 +1,105 @@
 import  * as d3 from 'd3'
 namespace Chart {
 
+  type Point = {
+    x: number
+    y: number
+  }
+
   type Asymptotes = {
-    data: number[]
     color: string
+    points: Array<Point>
    }
+
   interface SplineProperty {
     yLabel?: string[]
     xLabel?: string[]
     asymptotes?: Array<Asymptotes>
+    grid?: {
+      size?: number
+    }
+    log?: boolean
   }
 
   export class Spline {
     yLabel: string[]
     xLabel: string[]
-    target: string
+    log: boolean = false
+    grid: {size: number}
     path: d3.Path = d3.path()
     asymptotes: Array<Asymptotes>
-    constructor(target: string, {yLabel, xLabel, asymptotes}: SplineProperty) {
-      this.target = target
+    target: d3.Selection<d3.BaseType, unknown, HTMLElement, any>
+    scene: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>
+    constructor(target: string,
+      {
+        yLabel,
+        xLabel,
+        asymptotes,
+        grid,
+        log,
+      }: SplineProperty) {
+      this.log = log
+      this.target = d3.select(target)
+      this.scene = this.target.append('svg').attr('id', 'scene')
       this.yLabel = yLabel
+      this.grid = {
+        size: grid.size
+      }
       this.xLabel = xLabel
       this.asymptotes = asymptotes
+    
+    }
+    public render() {
+      if (this.xLabel && this.yLabel) this.drawGrid()
+      if (this.yLabel) {
+        this.scene
+        .attr('height', this.yLabel.length * this.grid.size - 55)
+        this.renderYLabel(this.target)
+      }
+    
+
+    }
+    private renderYLabel(target: d3.Selection<d3.BaseType, unknown, HTMLElement, any>) {
+
+        this.scene
+          .append('g')
+          .attr('transform', `translate(${this.grid.size}, 10)`)
+          .selectAll('text')
+          .data(this.yLabel)
+          .enter()
+          .append('text')
+          .attr('text-anchor', 'end')
+          .attr('font-size', '10px')
+          .attr('y', (_, i): number => {
+            return i * (56 - i)
+          })
+          .text(d => {return d})
+
+    }
+
+    private drawGrid() {
+      const scopedGroup = this.scene.append('g')
+      scopedGroup
+        .attr('transform', `translate(${this.grid.size + 10}, 0)`)
+        .append('defs')
+
+        .append('pattern')
+        .attr('id', 'p10')
+        .attr('width', this.grid.size)
+        .attr('height', this.grid.size)
+        .attr('patternUnits', 'userSpaceOnUse')
+
+        .append('path')
+        .attr('d', 'M 56 0 L 0 0 0 56')
+        .attr('fill', 'none')
+        .attr('stroke', 'skyblue')
+        .attr('stroke-width', '1')
+
+      scopedGroup
+        .append('rect')
+        .attr('width', '100%')
+        .attr('height', '100%')
+        .attr('fill', 'url(#p10)')
     }
   }
 
